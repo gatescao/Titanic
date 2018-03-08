c4b1e6b391ddfba92596fba8239bc458d855f668
Gates Cao <jialeicao2017@u.northwestern.edu>
2018-03-08 15:47:30 -0600

    updates

--- scratch.R
+++ scratch.R
@@ -1,6 +1,7 @@
 ##Load libraries
 library(tidyverse)
 library(modelr)
+library(readr)
 
 ##Read in data
 train <- read_csv(file = "data/train.csv") %>% as_tibble()
@@ -9,9 +10,11 @@ test <- read_csv(file = "data/test.csv") %>% as_tibble()
 ##Drop unwanted variables
 train <- train %>% select(-PassengerId, -Name, -Ticket, -Cabin)
 
-##Handle missing values
+##Drop missing values
 colSums(is.na(train))
 train <- na.omit(train)
+test$Age[is.na(test$Age)] <- median(na.omit(test$Age))
+test$Fare[is.na(test$Fare)] <- median(na.omit(test$Fare))
 
 ##Split train into training and validation set
 set.seed(1)
@@ -49,7 +52,7 @@ train_10fold <- train_10fold %>%
 
 
 ##KNN
-##Data preparation
+###Data preparation
 training_knn <- training
 validation_knn <- validation
 training_knn <- training_knn %>%
@@ -59,7 +62,7 @@ validation_knn <- validation_knn %>%
   mutate(Sex = ifelse(Sex == "male", 1, 0),
          Embarked = sapply(Embarked, switch, "C" = 0, "Q" = 1, "S" = 2))
 
-##Modeling
+###Modeling
 library(class)
 train_X <- training_knn %>% select(-Survived)
 test_X <- validation_knn %>% select(-Survived)
@@ -82,6 +85,13 @@ lda_class <- lda_predict$class
 table(lda_class, validation_knn$Survived)
 mean(lda_class == validation_knn$Survived)
 
+lda_preds <- predict(lda_fit, test)$class
+lda_test <- test %>% 
+  mutate(Survived = lda_preds) %>%
+  dplyr::select(PassengerId, Survived)
+
+write_csv(lda_test, "lda_test.csv")
+
 ##QDA
 qda_fit <- qda(Survived ~., data = training)
 qda_fit
@@ -92,3 +102,4 @@ names(qda_predict)
 qda_class <- qda_predict$class
 table(qda_class, validation_knn$Survived)
 mean(qda_class == validation_knn$Survived)
+
